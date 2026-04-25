dc7020de01090cb0e6fd2335a5678545548368ea
OBS-Akuma <JuicerAkuma@gmail.com>
2026-04-24 22:24:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
       <Image
         src="/img/meowhehe.gif"
         alt="meow image"
-        width={600}
-        height={400}
+        width={200}
+        height={200}
       />
-      <h1>Hiya meow...</h1>
+      <h1 className="float">Hiya meow...</h1>
 
       <style jsx>{`
         .container {
@@ -26,6 +26,17 @@ export default function Home() {
         h1 {
           font-size: 64px;
           margin-top: 30px;
+        }
+        .float {
+          animation: float 3s ease-in-out infinite;
+        }
+        @keyframes float {
+          0%, 100% {
+            transform: translateY(0px);
+          }
+          50% {
+            transform: translateY(-10px);
+          }
         }
       `}</style>
     </div>
